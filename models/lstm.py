6d6a16a8846357b0d2b36d2591e76dc7d6b92a4c
Alta Futuris <rahulkulhalli@gmail.com>
2022-12-09 13:30:41 -0500

    LSTM loss is now explicitly accrued over timesteps

--- models/lstm.py
+++ models/lstm.py
@@ -6,30 +6,30 @@ import torch.nn.functional as F
 class SimpleLSTM(nn.Module):
     def __init__(self, vocab_size: int, n_hidden: int = 8, embedding_dims: int = 8):
         super(SimpleLSTM, self).__init__()
-        
+
         self.vocab_size = vocab_size
         self.n_hidden = n_hidden
         self._embedding_dims = embedding_dims
-        
+
         # embedding matrix will be (vocab, expected_dims). We fine-tune this during backprop too.
         self.embedding = torch.randn((self.vocab_size, self._embedding_dims), requires_grad=True)
         self.lstm = nn.LSTM(input_size=self._embedding_dims, hidden_size=self.n_hidden, num_layers=1, bias=False, batch_first=True)
         self.hidden2vocab = nn.Linear(self.n_hidden, vocab_size)
-    
+
     def init_hidden(self, batch_size: int):
         # create two zero vectors.
         # the dims are (1*num_layers, batch_size, num_hidden)
         h0 = torch.randn((1, batch_size, self.n_hidden), requires_grad=True)
         c0 = torch.randn((1, batch_size, self.n_hidden), requires_grad=True)
         return h0, c0
-        
+
     def forward(self, x, hidden):
+        # store (b, n_vocab) tensors.
+        outputs = []
+
         emb_x = self.embedding[x]                                   # (b, seq) -> (b, seq, features)
+
         out, (h_new, c_new) = self.lstm(emb_x, hidden)
-        
-        # out[:, -1, :] == h[0, :, :]
-        last_timestep_output = out[:, -1, :].view(-1, self.n_hidden)
-        
-        linear = self.hidden2vocab(last_timestep_output)
-        
+        linear = self.hidden2vocab(out)
+
         return linear, (h_new, c_new)
--- train_lstm.py
+++ train_lstm.py
@@ -10,94 +10,95 @@ from pathlib import Path
 
 
 if __name__ == "__main__":
-    
+
     batch_size = 8
     n_epochs = 5
     window_size = 4
     n_hidden_features = 128
     n_embedding_dims = 64
     save_every = 2
-    
-    txt_utils = TextUtils(Path("data/c_and_p.txt"), compute_counts=False, model_type='char') 
-    
+
+    txt_utils = TextUtils(Path("data/c_and_p.txt"), compute_counts=False, model_type='char')
+
     train_dataset = DatasetForLSTM(txt_utils, window_size=window_size, model_type='char', stride=1, mode='train')
     test_dataset = DatasetForLSTM(txt_utils, window_size=window_size, model_type='char', stride=1, mode='val')
-    
+
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
-    
+
     TRAIN = True
-    
+
     # batch_first is set to True.
     model = SimpleLSTM (
         vocab_size=len(txt_utils.char2ix),
         n_hidden=n_hidden_features,
         embedding_dims=n_embedding_dims
     )
-    
+
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
-    criterion = nn.NLLLoss()
-    
+    criterion = nn.CrossEntropyLoss()
+
     if TRAIN:
-        
+
         mean_loss = []
         for epoch_ix in range(1, n_epochs+1):
             if epoch_ix > 1:
                 # reduce lr by 20%.
                 optimizer.param_groups[0]['lr'] *= 0.95
-                
+
             h = model.init_hidden(batch_size=batch_size)
-            
+
             for iter_ix, (x, y) in enumerate(train_loader):
-                
+
                 h = tuple([e.data for e in h])
-            
+
                 optimizer.zero_grad()
-                
+
                 # (out -> (b, seq_len, n_features))
                 logits, h = model(x, h)
-                
-                out = F.log_softmax(logits, dim=1)
-                
-                loss = criterion(out, y)
-                
-                loss.backward()
-                
-                mean_loss.append(loss.detach().item())
-                
+
+                seq_loss = torch.tensor(0., requires_grad=True)
+
+                for i in range(window_size):
+                    seq_loss = seq_loss + criterion(logits[:, i, :], y[:, i])
+
+                seq_loss.backward()
+
+                mean_loss.append(seq_loss.detach().item())
+
                 if (iter_ix+1) % 5000 == 0:
                     print(f"epoch: {epoch_ix}, iteration: {iter_ix+1}, mean loss: {torch.tensor(mean_loss).mean()}")
-                        
+
                 optimizer.step()
-            
+
             if epoch_ix % save_every == 0:
                 print("Saving LSTM model.")
                 torch.save(model.state_dict(), Path(f"models/lstm_{epoch_ix}.pt"))
-        
+
         # save the model.
         torch.save(model.state_dict(), Path("models/lstm_final.pth"))
-        
+
         print(50*'+')
         print("TESTING PERFORMANCE...")
         print(50*'+')
-        
+
         mean_loss = []
         with torch.no_grad():
             for iter_ix, (x, y) in enumerate(test_loader):
                 h = model.init_hidden(batch_size=batch_size)
-                
+
                 # print(x.shape, y.shape)
-                
+
                 h = tuple([e.data for e in h])
-                
+
                 # (out -> (b, seq_len, n_features))
                 logits, h = model(x, h)
-                
+
                 out = F.log_softmax(logits, dim=1)
-                
+
                 loss = criterion(out, y)
-                
+
                 mean_loss.append(loss.detach().item())
-                
+
                 if (iter_ix+1) % 250 == 0:
                     print(f"epoch: {epoch_ix}, iteration: {iter_ix+1}, mean loss: {torch.tensor(mean_loss).mean()}")
